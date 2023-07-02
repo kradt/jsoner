faf6f4c8790613841e3173a143ef6fdee6232820
krad <mynamedark713@gmail.com>
2023-07-03 02:31:27 +0300

    add comments

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from pydantic import BaseModel
 
 
 class Jsoner:
-    
+    """
+        A class for saving arbitrary data models to a json file
+        :param models: list of model that will be saving 
+        :param filename: name of file where will be saving data
+        :param save_immediately: if true: saving will start after every append
+    """
     def __init__(
             self,
             models: list[BaseModel] | BaseModel,
@@ -11,18 +16,29 @@ class Jsoner:
             save_immediately: bool = False):
         
         self.models = models
+        if not filename.endswith(".json"):
+            raise ValueError("You should use only json files")
+        
         self.filename = filename
         self.save_immediately = save_immediately
 
         self.objects_to_save = []
 
-    def _belong_to_models(self, model) -> bool:
+    def _belong_to_models(self, obj: BaseModel) -> bool:
+        """
+            Check if model is instance of some model from self.models
+            :param obj: Object of some model
+        """
         for m in self.models:
-            if isinstance(model, m):
+            if isinstance(obj, m):
                 return True
         return False
     
     def append(self, obj: BaseModel) -> None:
+        """
+            Add object to list of objects
+            :param obj: Object of some model
+        """
         if not self._belong_to_models(obj):
             raise ValueError("You can't use underfinded models")
         self.objects_to_save.append(obj.dict())
@@ -31,6 +47,10 @@ class Jsoner:
             self.save()
 
     def _get_json_from_file(self) -> dict:
+        """
+            Extract data from json file to python dict 
+            :return data: dict from json file
+        """
         with open(self.filename, "r") as file:
             file = file.read()
             if not file:
@@ -39,6 +59,10 @@ class Jsoner:
         return data
 
     def save(self, indent: int = 4, ensure_ascii: bool = True, allow_nan: bool = True):
+        """
+            Save objects to json file
+            params equal to json.dump params
+        """
         jsn = self._get_json_from_file()
         with open(self.filename, "w") as file:
             if isinstance(jsn, list):
@@ -49,4 +73,3 @@ class Jsoner:
 
             jsn = jsn if jsn else self.objects_to_save
             json.dump(jsn, file, indent=indent, ensure_ascii=ensure_ascii, allow_nan=allow_nan)
-
