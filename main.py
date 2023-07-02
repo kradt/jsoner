cf8f03ed477b137d2e59fffd29a3f85c4ef0cc2c
krad <mynamedark713@gmail.com>
2023-07-03 01:58:03 +0300

    add save to jsoner

--- main.py
+++ main.py
@@ -22,19 +22,31 @@ class Jsoner:
                 return True
         return False
     
-    def append(self, obj):
+    def append(self, obj: BaseModel) -> None:
         if not self._belong_to_models(obj):
             raise ValueError("You can't use underfinded models")
-        self.objects_to_save.append(obj)
+        self.objects_to_save.append(obj.dict())
 
         if self.save_immediately:
             self.save()
 
-    def _get_object_from_file(self):
+    def _get_json_from_file(self) -> dict:
         with open(self.filename, "r") as file:
-            data = json.load(file)
+            file = file.read()
+            if not file:
+                return None
+            data = json.loads(file)
         return data
 
-    def save(self):
-        pass
+    def save(self, indent: int = 4, ensure_ascii: bool = True, allow_nan: bool = True):
+        jsn = self._get_json_from_file()
+        with open(self.filename, "w") as file:
+            if isinstance(jsn, list):
+                jsn.extend(self.objects_to_save) if jsn else None
+
+            if isinstance(jsn, dict):
+                jsn.update(self.objects_to_save) if jsn else None
+
+            jsn = jsn if jsn else self.objects_to_save
+            json.dump(jsn, file, indent=indent, ensure_ascii=ensure_ascii, allow_nan=allow_nan)
 
